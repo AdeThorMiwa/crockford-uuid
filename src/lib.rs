ad5b1f07aacb6c63e0378d89b0b1c8cc7b1cbb29
AdeThorMiwa <bendamyth@gmail.com>
2023-05-25 18:39:43 +0100

    add test to compare with string

--- src/lib.rs
+++ src/lib.rs
@@ -113,6 +113,15 @@ pub mod crock_ford {
             self.value_with_checksum() == other.value_with_checksum()
         }
     }
+
+    impl PartialEq<String> for Uuid {
+        fn eq(&self, other: &String) -> bool {
+            match Uuid::from_str(other) {
+                Ok(uuid) => *self == uuid,
+                Err(_) => false,
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -148,7 +157,12 @@ mod tests {
         assert_ne!(first, second);
     }
 
-    // compare with string
+    #[test]
+    fn compare_uuid_with_string() {
+        let str_uuid = "1fe1ewyb60gvfj71yd4aq1qftz5dkwkjg";
+        let uuid: Uuid = str_uuid.try_into().unwrap();
+        assert_ne!(uuid, str_uuid.to_string());
+    }
 
     // get as integer
 
